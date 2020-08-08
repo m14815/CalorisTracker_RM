5166f1cf653d28ada5922816f2f097aa153d44bc
Ray <m14815@gmail.com>
2020-08-08 12:13:06 +0800

    Initialize the new project, part 1, build up restful web service

---
+++ src/main/java/com/example/restful_CT/RestfulCtApplication.java
@@ -0,0 +1,13 @@
+package com.example.restful_CT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulCtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulCtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restful_CT/RestfulCtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_CT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulCtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
